da5cf2f40c533cdf431455d4788e998eb61102c0
Ampplex <team.amplex@gmail.com>
2025-02-22 22:18:47 +0530

    Added error handler in signup client

--- server/controllers/authentication/client_auth/auth.js
+++ server/controllers/authentication/client_auth/auth.js
@@ -29,21 +29,27 @@ const handleSignUp = async (req, res) => {
     return res.status(404).json({ msg: "All fields are required" });
   }
 
-  const result = await Client.create({
-    client_name: body.client_name,
-    email: body.email,
-    password: body.password,
-    linkedin_url: body.linkedin_url,
-    instagram_url: body.instagram_url,
-    isAvailable: true,
-    business_name: body.business_name,
-    description: "",
-    title: ""
-  });
+  try {
+    const result = await Client.create({
+      client_name: body.client_name,
+      email: body.email,
+      password: body.password,
+      linkedin_url: body.linkedin_url,
+      instagram_url: body.instagram_url,
+      isAvailable: true,
+      business_name: body.business_name,
+      description: "",
+      title: ""
+    });
+  
+    return res
+      .status(201)
+      .json({ msg: "Client created successfully", id: result._id });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ error: err });
+  }
 
-  return res
-    .status(201)
-    .json({ msg: "Client created successfully", id: result._id });
 };
 
 module.exports = {
